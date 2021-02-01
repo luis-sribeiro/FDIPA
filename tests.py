7ea561a1d4de0afe52ae4e0348a67710455a29b6
Vitor Monteiro <vitormgou@gmail.com>
2021-02-01 13:57:50 -0300

    Add coluna de "uns" nos dados de treinamento

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ from NeuralNet import NeuralNet
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn import datasets
+import numpy as np
 
 
 #Color map for plotting figures
@@ -20,6 +21,9 @@ plt.xlim(-1.5,1.5)
 plt.scatter(X_train[:,0], X_train[:,1], c = y_train, cmap=cmap)
 plt.show()
 
+#Add coluna de uns na entrada de treinamento
+uns = np.ones((np.shape(X_train)[0],1))
+X_train_aux = np.concatenate((uns, X_train), axis = 1)
 
 net = NeuralNet([2,5,1])
 
