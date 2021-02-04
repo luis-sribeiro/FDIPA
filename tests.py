87ca59b2b941162531207e2e7d00b3d37cb85481
Vitor Monteiro <vitormgou@gmail.com>
2021-02-04 17:43:21 -0300

    Olhar a consulta no mini_batch

--- tests.py
+++ tests.py
@@ -15,15 +15,12 @@ matplotlib.style.use('ggplot')
 #Creating the training data
 X_train , y_train = datasets.make_circles(n_samples=500, random_state = 10,noise=0.15, factor=0.3)
 
+
 plt.figure(figsize=(19.20,10.80))
 plt.ylim(-1.5,1.5)
 plt.xlim(-1.5,1.5)
 plt.scatter(X_train[:,0], X_train[:,1], c = y_train, cmap=cmap)
 plt.show()
-
-#Add coluna de uns na entrada de treinamento
-uns = np.ones((np.shape(X_train)[0],1))
-X_train_aux = np.concatenate((uns, X_train), axis = 1)
 
 net = NeuralNet([2,5,1])
 
