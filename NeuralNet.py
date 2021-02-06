344c2ddb37cb91ecae4af747b1e50c71840f980a
Vitor Monteiro <vitormgou@gmail.com>
2021-02-06 13:28:47 -0300

    Verificar backprop

--- NeuralNet.py
+++ NeuralNet.py
@@ -7,6 +7,7 @@ Created on Sat Feb 8 13:28:51 2020
 
 import numpy as np
 import random
+import copy as cp
 
 #classe que representa uma rede neural
 class NeuralNet(object):
@@ -35,6 +36,7 @@ class NeuralNet(object):
     def feedforward(self, a):
         #força o primeiro neuronio de cada camada ser 1 uma vez que representa o bias
         a = np.concatenate(([1], a), axis=0)
+        a = a.T
         for i in range(0,len(self.weights)-1):
             a = sigmoid(np.dot(self.weights[i], a))
             a[0] = 1
@@ -49,7 +51,6 @@ class NeuralNet(object):
         if test_data:
             test_data = list(test_data)
             n_test = len(test_data)
-        
         for j in range(epochs):
             random.shuffle(training_data)
             mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
@@ -70,64 +71,12 @@ class NeuralNet(object):
 
         nabla_w = [np.zeros(w.shape) for w in self.weights]
  
-        #mini_batch = [X_Teste, y_teste]
         for x, y in mini_batch:
             delta_nabla_w = self.backprop(x, y)            
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
         
         
         self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
-
-    def backpropBloco(self, mini_batch):
-        """Retorna `nabla_w` representando o
-         gradiente para a função de custo C_x. `nabla_w` é uma lista de camadas de matrizes numpy,
-         semelhante a  `self.weights`."""
-
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-        
-        # Retirar do mini_batch o conjunto dos x's de entrada para montar  o activation com
-        # a primeira linha formada por 1's
-        # Feedforward
-        activation = mini_batch[0]
-        uns = np.ones((np.shape(activation)[0],1))
-        activation = np.concatenate((uns, X_train), axis = 1)
-        activation = activation.T
-
-        y = mini_batch[1]
-        # Lista para armazenar todas as ativações, camada por camada
-        activations = [activation] 
-
-        # Lista para armazenar todos os vetores z, camada por camada
-        zs = [] 
-        for i in range(0,len(self.weights)-1):
-            w = self.weights[i]
-            z = np.dot(w, activation)
-            zs.append(z)
-            activation = sigmoid(z)
-            activation[0,:] = 1
-            activations.append(activation)
-
-        w = self.weights[-1]
-        z = np.dot(w, activation)
-        zs.append(z)
-        activation = sigmoid(z)
-        activations.append(activation)
-
-        
-        # Backward pass
-        
-        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-        
-        # Aqui, l = 1 significa a última camada de neurônios, l = 2 é a
-        # segunda e assim por diante. 
-        for l in range(2, self.num_layers):
-            z = zs[-l]
-            sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-            
-        return nabla_w        
 
     def backprop(self, x, y):
         """Retorna `nabla_w` representando o
@@ -137,10 +86,10 @@ class NeuralNet(object):
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         
         # Feedforward
-        activation = np.zeros(len(x)+1)
-        activation[0] = 1
-        activation[1:] = x
-
+        activation = np.zeros((1,len(x)+1))
+        activation[0,0] = 1
+        activation[0,1:] = x
+        activation = activation.T
         # Lista para armazenar todas as ativações, camada por camada
         activations = [activation] 
 
@@ -159,7 +108,6 @@ class NeuralNet(object):
         zs.append(z)
         activation = sigmoid(z)
         activations.append(activation)
-
         
         # Backward pass
         delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
@@ -174,6 +122,53 @@ class NeuralNet(object):
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
             
         return nabla_w
+
+    def backpropFDIPA(self, w, mini_batch):
+        """Retorna `nabla_w` representando o
+         gradiente para a função de custo C_x. `nabla_w` é uma lista de camadas de matrizes numpy,
+         semelhante a  `self.weights`."""
+
+        nabla_w = [np.zeros(waux.shape) for waux in w]
+        
+        # Retirar do mini_batch o conjunto dos x's de entrada para montar o activation com
+        # a primeira linha formada por 1's
+        # Feedforward
+        activation = mini_batch[0]
+        uns = np.ones((np.shape(activation)[0],1))
+        activation = np.concatenate((uns, activation), axis = 1)
+        activation = activation.T
+
+        y = mini_batch[1]
+        # Lista para armazenar todas as ativações, camada por camada
+        activations = [activation] 
+
+        # Lista para armazenar todos os vetores z, camada por camada
+        zs = [] 
+        for i in range(0,len(w)-1):
+            z = np.dot(w[i], activation)
+            zs.append(z)
+            activation = sigmoid(z)
+            activation[0,:] = 1
+            activations.append(activation)
+
+        z = np.dot(w[-1], activation)
+        zs.append(z)
+        activation = sigmoid(z)
+        activations.append(activation)
+        
+        # Backward pass
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        
+        # Aqui, l = 1 significa a última camada de neurônios, l = 2 é a
+        # segunda e assim por diante. 
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(w[-l+1].transpose(), delta) * sp
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+            
+        return nabla_w        
 
     def evaluate(self, test_data):
         """Retorna o número de entradas de teste para as quais a rede neural 
@@ -200,27 +195,31 @@ class NeuralNet(object):
             end = start + w.size
             weightsAux.append(v[start:end].reshape(w.shape))
             start = end
+        return weightsAux
             
     #Nesse caso, deverá ser realizada a transformacao antes
     #de fazer a computacao da rede
     #To Do: obter o tamanho do data set, decidir como o x e o y serão passados para decidir como o calculo será feito
-
+    #retorna f e g pro FDIPA (fun)
     def feedforwardFDIPA(self, w, mini_batch):
-        n = mini_batch[0].shape[0] #Tamanho do dataset 
+        n = mini_batch[0].shape[0] #Tamanho do mini_batch
 
         a = cp.copy(mini_batch[0])
         uns = np.ones((np.shape(a)[0],1))
         a = np.concatenate((uns, a), axis = 1)
         a = a.T
-        
-        start = 0
-        for i in range(0, len(self.weights)-1):
-            end = start + self.weights[i].size
-            a = sigmoid(np.dot(w[0,start:end].reshape(self.weights[i].shape) ,a))
-            start = end
+        y = mini_batch[1]
+
+        for i in range(0, len(w)-1):
+            a = sigmoid(np.dot(w[i] ,a))
             a[0 , :] = 1
-        a = sigmoid(np.dot(w[0,start:], a))
+        a = sigmoid(np.dot(w[-1], a))
         return (1/(2*n))*np.linalg.norm(a-y,'fro')**2, -(np.linalg.norm(w)**2)/2
+
+    #retorna df e dg pro FDIPA(dfun)
+    def dfunFDIPA(self, w, mini_batch):
+        #recebe w como uma lista de matrizes
+        return self.backpropFDIPA(w, mini_batch), -mat2vet(w)    
 
 # funcao de ativacao sigmoide
 def sigmoid(z):
@@ -229,5 +228,9 @@ def sigmoid(z):
 # Função para retornar as derivadas da função Sigmóide
 def sigmoid_prime(z):
     return sigmoid(z)*(1-sigmoid(z))
+
+#converte matriz para vetor
+def mat2vet(w):
+    return np.concatenate(list(map(lambda a: a.reshape(-1), w))) #concatena lista de arrays
 
 rede1 = NeuralNet([2,3,1])
--- tests.py
+++ tests.py
@@ -20,8 +20,26 @@ plt.figure(figsize=(19.20,10.80))
 plt.ylim(-1.5,1.5)
 plt.xlim(-1.5,1.5)
 plt.scatter(X_train[:,0], X_train[:,1], c = y_train, cmap=cmap)
-plt.show()
+#plt.show()
+#Olhar como definir o dataSet pra entrada pra rede neural
+#dataSet = [X_train, y_train]
+
+#Acredito que essa seja a forma certa de criar o dataSet
+#Dentro da funcao SGD ele transofrma essa entrada em uma lista
+dataSet = zip(X_train, y_train)
+dataSet1 = list(dataSet)
+
+#for x,y in dataSet:
+#    print("x: %r \t y = %r"% (x, y))
 
 net = NeuralNet([2,5,1])
 
 net.feedforward(X_train[0])
+net.SGD(dataSet, 100, 25, 0.8)
+
+print("fim")
+
+#Teste:
+for x,y in dataSet1:
+    r = net.feedforward(x)
+    print("x: %r \t y = %r"% (r, y))
